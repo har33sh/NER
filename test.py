67e3faf056c0d4ec9d4fbd467b7d22d91aef61d9
hareesh <hareesh@SnowBall.local>
2020-05-16 19:47:11 +0530

    moving to single file

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-import pandas as pd
 import numpy as np
+import pickle
 
 from keras.models import load_model
+from keras.preprocessing.sequence import pad_sequences
 
 FILE_PATH = "Training-data.csv"
 
@@ -21,16 +22,42 @@ def pred2label(pred):
     return out
 
 model = load_model("model.h5")
-s = "lets meet around 12 tommorrow"
 
-test_x = [[word2idx[w] for w in s.split(" ")]]
-test_x = pad_sequences(maxlen=max_len, sequences=test_x, padding="post", value=n_words - 1)
+info_testing = pickle.load(open( "info_testing.p", "rb"))
+idx2tag = info_testing['idx2tag']
+word2idx = info_testing['word2idx']
+tag2idx = info_testing['tag2idx']
+words = info_testing['words']
+tags = info_testing['tags']
+max_len = info_testing['max_len']
+n_words = info_testing['n_words']
 
-result = model.predict(np.array([test_x[0]]))
-result = np.argmax(result, axis=-1)
+while True:
+    try:
+        print ("Enter sentence for NER..")
+        sentence = raw_input()
+        test_x = [[word2idx[w] for w in sentence.split(" ")]]
+        print(test_x)
+        # test_x = []
+        # for w in sentence.split(" "):
+        #     x = []
+        #     if word2idx.get(w):
+        #         x.append(word2idx.get(w))
+        #     else:
+        #         x.append(word2idx.get("."))
+        #     test_x = [x]
 
-print("{:15} ({:5}): {}".format("Word", "True", "Pred"))
-for w, pred in zip(test_x[0], result[0]):
-    print("{:15}: {}".format(words[w], tags[pred]))
+        test_x = pad_sequences(maxlen=max_len, sequences=test_x, padding="post", value=n_words - 1)
+        print("chech")
+        result = model.predict(np.array([test_x[0]]))
+        print("dfgd")
+        result = np.argmax(result, axis=-1)
 
+        print("{:15} ({:5}): {}".format("Word", "True", "Pred"))
+        for w, pred in zip(test_x[0], result[0]):
+            if words[w] == 'ENDPAD':
+                break
+            print("{:15}: {}".format(words[w], tags[pred]))
+    except Exception as error:
+        print("Error %s", error)
 
