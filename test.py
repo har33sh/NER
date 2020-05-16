a6946c86ec5d767a7d2525f171858ec1796ce987
hareesh <hareesh@SnowBall.local>
2020-05-16 20:05:45 +0530

    almost there

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pickle
 
-from keras.models import load_model
 from keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
 
-FILE_PATH = "Training-data.csv"
+MODEL_PATH = "model.h5"
+TRAIN_INFORMATION = "info_testing.p"
 
 def pred2label(pred):
     """
@@ -21,43 +22,52 @@ def pred2label(pred):
         out.append(out_i)
     return out
 
-model = load_model("model.h5")
+try:
+    print("Loading NER model...")
+    model = load_model(MODEL_PATH, compile=True)
+except Exception as error:
+    print("Error while loading model, does the model exist ?")
+else:
+    print("Model loaded successfully")
 
-info_testing = pickle.load(open( "info_testing.p", "rb"))
-idx2tag = info_testing['idx2tag']
-word2idx = info_testing['word2idx']
-tag2idx = info_testing['tag2idx']
-words = info_testing['words']
-tags = info_testing['tags']
-max_len = info_testing['max_len']
-n_words = info_testing['n_words']
+try:
+    print("Loading information used while training")
+    info_testing = pickle.load(open(TRAIN_INFORMATION, "rb"))
+    idx2tag = info_testing['idx2tag']
+    word2idx = info_testing['word2idx']
+    tag2idx = info_testing['tag2idx']
+    words = info_testing['words']
+    tags = info_testing['tags']
+    max_len = info_testing['max_len']
+    n_words = info_testing['n_words']
+except Exception as error:
+    print("Error while loading information, does the file exist ?")
+else:
+    print("Information loaded successfully")
 
 while True:
     try:
         print ("Enter sentence for NER..")
         sentence = raw_input()
-        test_x = [[word2idx[w] for w in sentence.split(" ")]]
-        print(test_x)
-        # test_x = []
-        # for w in sentence.split(" "):
-        #     x = []
-        #     if word2idx.get(w):
-        #         x.append(word2idx.get(w))
-        #     else:
-        #         x.append(word2idx.get("."))
-        #     test_x = [x]
-
-        test_x = pad_sequences(maxlen=max_len, sequences=test_x, padding="post", value=n_words - 1)
-        print("chech")
-        result = model.predict(np.array([test_x[0]]))
-        print("dfgd")
-        result = np.argmax(result, axis=-1)
-
-        print("{:15} ({:5}): {}".format("Word", "True", "Pred"))
-        for w, pred in zip(test_x[0], result[0]):
-            if words[w] == 'ENDPAD':
+        for word in sentence.split(" "):
+            if word2idx.get(word) is None:
+                print("Word not found ", word)
+                print("Try again..")
                 break
-            print("{:15}: {}".format(words[w], tags[pred]))
-    except Exception as error:
-        print("Error %s", error)
 
+        else:
+
+            test_x = [[word2idx[w] for w in sentence.split(" ")]]
+            test_x = pad_sequences(maxlen=max_len, sequences=test_x, padding="post", value=n_words - 1)
+
+            result = model.predict(np.array([test_x[0]]))
+            result = np.argmax(result, axis=-1)
+
+            print("{:15} ({:5}): {}".format("Word", "True", "Pred"))
+            for w, pred in zip(test_x[0], result[0]):
+                if words[w] == 'ENDPAD':
+                    break
+                print("{:15}: {}".format(words[w], tags[pred]))
+
+    except Exception as error:
+        print("Some Error occurred %s", error)
